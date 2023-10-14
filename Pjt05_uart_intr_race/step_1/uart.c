4323651f4ab502a8a7203687726b5645ba956a1a
Taek_2 <118153233+taek2222@users.noreply.github.com>
2023-10-14 15:39:03 +0900

    Part 5. Uart_Intr_Race

---
+++ Pjt05_uart_intr_race/step_1/uart.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <avr/io.h>
+#include <compat/deprecated.h>
+#include <util/delay.h>
+#include "uart.h"
+
+void uart_init()
+{
+	UBRR0H = 0x00; UBRR0L = 0x07;
+	UCSR0A |= (1 << U2X0);
+	UCSR0B |= (1 << TXEN0);
+	UCSR0B |= (1 << TXCIE0);
+}
+
+char buf[64];
+int volatile bufi, txend = 1;
+
+void uart_putstart(char str[])
+{
+	char ch; int n = strlen(str);
+	while(sizeof(buf) - strlen(buf + bufi) - 1 < n);
+	strcpy(buf, buf + bufi); strcat(buf, str);
+	bufi = 0;
+	
+	if (txend) {
+		txend = 0; ch = buf[bufi++];
+		UDR0 = ch;
+	}
+}
---
+++ Pjt05_uart_intr_race/step_1/uart.h
@@ -0,0 +1,4 @@
+void uart_putstart(char str[]);
+void uart_init();
+extern char buf[64];
+extern int volatile bufi, txend;
